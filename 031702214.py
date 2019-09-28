71e7a9567ffd8b5b65f5fcf75446116a76610c88
0pyj0 <724326783@qq.com>
2019-09-28 23:45:46 +0800

    第一次个人编程作业

--- 031702214.py
+++ 031702214.py
@@ -175,11 +175,8 @@ def fseven():
     finallist.append(string)
     final["地址"]=finallist
 
-while 1:
-    try:
+try:
     mlist=input()
-    if(mlist=="EOF"):
-        break
     mlist.split('\\n')
     #from ast import literal_eval
     #mlist=literal_eval(ll)#将文件中的内容转换为list
@@ -225,4 +222,4 @@ while 1:
     #print(final)
     print(json.dumps(final))
 except EOFError:
-    main(inputraw)
+    print("输入输出错误")
